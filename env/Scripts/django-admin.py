0be94168b777b5d35b2c27cbaef5ad80be64f2e3
ElimuStuart <stuartelimu@gmail.com>
2018-10-29 18:51:28 +0300

    Initial commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\stuar\Desktop\Dev\Web\Django\todo_project\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
