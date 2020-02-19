13210e80ef746e4b827e9c8aeb76a93a4594b2b9
ElimuStuart <stuartelimu@gmail.com>
2020-02-19 19:54:18 +0300

    add .gitignore file

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\stuar\Desktop\Dev\Web\Django\todo_project\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
